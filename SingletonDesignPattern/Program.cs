b2c7728958a96c0f0a5db01ccef54b9d95201ab8
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-04-25 17:39:57 +0530

    Add project files.

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(First, Second);
+        }
+
+        private static void Second()
+        {
+            Console.WriteLine("Second thread started.");
+            Singleton singleton2 = Singleton.Instance;
+            singleton2.SomeMethod("Second");
+        }
+
+        private static void First()
+        {
+            Singleton singleton1 = Singleton.Instance;
+            Console.WriteLine("First thread started.");
+            singleton1.SomeMethod("First");
+        }
+    }
+}
---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+        // Private constructor to prevent instantiation from outside
+        private Singleton()
+        {
+            Console.WriteLine("Singleton instance created.");
+        }
+        // Public method to provide access to the instance
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        public void SomeMethod(string msg)
+        {
+            Console.WriteLine("Message : " + msg);
+        }
+    }
+}
