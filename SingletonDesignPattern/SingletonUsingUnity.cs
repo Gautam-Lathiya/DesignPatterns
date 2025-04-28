f2641fe2b537b376daecfe6ac229ef7d4ea7f3a2
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-04-28 18:07:33 +0530

    Singleton using Unity Package

---
+++ SingletonDesignPattern/SingletonUsingUnity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace SingletonDesignPattern
+{
+    class SingletonUsingUnity
+    {
+        public void Display()
+        {
+            Console.WriteLine("Singleton using Unity");
+        }
+    }
+
+    class UnitySingletonTest
+    {
+        static void Main(string[] args)
+        {
+            // Create a Unity container
+            var container = new Unity.UnityContainer();
+
+            // Register the singleton type
+            container.RegisterSingleton<SingletonUsingUnity>();
+
+            // Resolve the singleton instance
+            var singletonInstance1 = container.Resolve<SingletonUsingUnity>();
+            var singletonInstance2 = container.Resolve<SingletonUsingUnity>();
+
+            // Check if both instances are the same
+            Console.WriteLine(object.ReferenceEquals(singletonInstance1, singletonInstance2)); // True
+
+            // Call the display method
+            singletonInstance1.Display();
+        }
+    }
+}
